3f812c47f3d7aea33ef26a7e70f8272122020740
Alena Smagliy <smagliyalena@gmail.com>
2022-08-14 02:54:58 +0400

    Refactor game

--- script.js
+++ script.js
@@ -1,67 +1,70 @@
-function playRound() {
-    let playerSelection = (prompt('Введите свою фигуру')).toLocaleLowerCase();
-    function getComputerChoice() {
-        let c;
-        let i = Math.floor(Math.random() * 3);
-        if (i === 0) {
-            c = 'rock';
-        }
-        else if (i === 1) {
-            c = 'paper';
-        }
-        else if (i === 2) {
-            c = 'scissors';
-        }
-        return c;
-        } 
-    let computerChoice = getComputerChoice();
-    let result
-    let winsCount
-    switch (true) {
-        case (playerSelection == computerChoice):
-            result = 'Draw';
-            winsCount = 0
-            break;
-        case (playerSelection == 'rock') && (computerChoice == 'paper'):
-            result = 'You Lose! Paper beats Rock'
-            winsCount = 0
-            break;
-        case (playerSelection == 'scissors') && (computerChoice == 'rock'):
-            result = 'You Lose! Rock beats Scissors'
-            winsCount = 0
-            break;
-        case (playerSelection == 'paper') && (computerChoice == 'scissors'):
-            result = 'You Lose! Scissors beats Paper'
-            winsCount = 0
-            break;
-        case (playerSelection == 'paper') && (computerChoice == 'rock'):
-            result = 'You Won! Paper beats Rock'
-            winsCount = 1
-            break;
-        case (playerSelection == 'rock') && (computerChoice == 'scissors'):
-            result = 'You Won! Rock beats Scissors'
-            winsCount = 1
-            break;
-        case (playerSelection == 'scissors') && (computerChoice == 'paper'):
-            result = 'You Won! Scissors beats Paper'
-            winsCount = 1
-            break;
-    }
-    return result
-    return winsCount
-}
-//console.log(playRound())
-let gameScore = 0
-function game() {
-    for (let i=0; i<5; i++) {
-        console.log(playRound(i))
-        
-    }
-    gameScore =  gameScore + winsCount
-    return gameScore
-}
-console.log(game())
-//console.log(gameScore)
+"use strict";
 
+let playerScore = 0;
+let computerScore = 0;
 
+function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random() * 3);
+        switch (randomNumber) {
+            case 0: 
+                return 'rock';
+            case 1:
+                return 'paper';
+            case 2:
+                return 'scissors';
+        }
+}
+
+function hasWon(selection1, selection2) {
+    if (selection1 == selection2) {
+        return 0
+    }
+
+    if (
+        selection1 == 'rock' && selection2 == 'paper' ||
+        selection1 == 'scissors' && selection2 == 'rock' ||
+        selection1 == 'paper' && selection2 == 'scissors'
+    ) {
+        return -1;
+    }
+
+    return 1;
+}
+
+function playRound(playerSelection, computerSelection) {
+    if (hasWon(playerSelection, computerSelection) == 0) {
+        return 'Tie round'
+    }
+
+    if (hasWon(playerSelection, computerSelection) == -1) {
+        computerScore++;
+        return `You Lose! ${computerSelection.toUpperCase()} beats ${playerSelection.toUpperCase()}`;
+    }
+    
+    if (hasWon(playerSelection, computerSelection) == 1) {
+        playerScore++;
+        return `You Won! ${playerSelection.toUpperCase()} beats ${computerSelection.toUpperCase()}`;
+    }
+}
+
+//let playerSelection1 = prompt('Insert your choice').toLocaleLowerCase();
+//let computerSelection2 = getComputerChoice();
+
+for (let i = 0; i < 5; i++) {
+    let playerSelection1 = prompt('Insert your choice').toLocaleLowerCase();
+    let computerSelection2 = getComputerChoice();
+    let result = playRound(playerSelection1,computerSelection2);
+
+    console.log(result);
+    console.log(playerScore);
+}
+if (playerScore > computerScore) {
+    console.log('You won a game');
+}
+else if (playerScore < computerScore) {
+    console.log('You lose a game');
+}
+else {
+    console.log('Tie game');
+}
 //rock
